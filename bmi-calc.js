8617c8825be47064c8d6d205500da67d88bf9afc
hiroki_nomura <hiroki.nomura@hakuhodo.co.jp>
2021-11-08 10:05:25 +0900

    First Commit.

---
+++ bmi-calc.js
@@ -0,0 +1,9 @@
+function calcBmi() {
+    var height = document.getElementById("height").value
+    var weight = document.getElementById("weight").value
+    var bmi = weight / Math.pow(height / 100, 2)
+    document.getElementById("bmi").value = bmi
+}
+
+
+
